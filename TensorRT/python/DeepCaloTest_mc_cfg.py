73c6b59bfa29bfe3b0d07834d1bfdd48636216b1
Keb-L <kelvin.lin1@gmail.com>
2020-03-25 14:54:59 -0700

    Update DeepCaloTest_mc_cfg.py

--- TensorRT/python/DeepCaloTest_mc_cfg.py
+++ TensorRT/python/DeepCaloTest_mc_cfg.py
@@ -47,7 +47,7 @@ process.GlobalTag.globaltag = cms.string('100X_upgrade2018_realistic_v10')
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(options.maxEvents) )
 process.source = cms.Source("PoolSource",
     #fileNames = cms.untracked.vstring('file:../../Core/data/store_mc_RunIISpring18MiniAOD_BulkGravTohhTohbbhbb_narrow_M-2000_13TeV-madgraph_MINIAODSIM_100X_upgrade2018_realistic_v10-v1_30000_24A0230C-B530-E811-ADE3-14187741120B.root')
-    	fileNames = cms.untracked.vstring(['file:../../Core/data/skim.root']*500),
+    	fileNames = cms.untracked.vstring(['file:../../Core/data/skim.root']*100),
 	duplicateCheckMode = cms.untracked.string('noDuplicateCheck')
 )
 
